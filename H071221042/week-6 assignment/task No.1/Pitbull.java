109c149f4d7078cc089b7a6d26520050d3ec4022
KELVINCDVSC <kelvin74529@gmail.com>
2023-05-05 15:23:04 +0800

    Menambah tugas pekan 6

---
+++ H071221042/week-6 assignment/task No.1/Pitbull.java
@@ -0,0 +1,9 @@
+public class Pitbull extends Dog  {
+    public void move() {
+        position += 3;
+    }
+
+    void describe() {
+        System.out.println("Anjing Pittbull adalah anjing yang dikenal kesetiannya pada majikannya.Pittbull juga dikenal sebagai anjing yang berukuran cukup besar dengan warna beragam seperti coklat, hitam, abu-abu, putih, dan lain-lain.\n");
+    }
+}
